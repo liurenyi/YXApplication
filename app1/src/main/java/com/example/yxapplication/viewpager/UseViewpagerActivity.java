8ff984a184dd9a4076f72573bec2f9a14860e899
zhuqg <369793561@qq.com>
2018-01-22 11:21:58 +0800

    2018/01/22 11:21
    1.优化下

--- app1/src/main/java/com/example/yxapplication/viewpager/UseViewpagerActivity.java
+++ app1/src/main/java/com/example/yxapplication/viewpager/UseViewpagerActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 
 import com.example.yxapplication.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UseViewpagerActivity extends AppCompatActivity {
 
@@ -19,9 +24,12 @@ public class UseViewpagerActivity extends AppCompatActivity {
     private ViewPagerAdapter adapter;
     private View view;
     private ImageView image;
+    private GridView gridView;
+    private List<Map<String, Object>> data_list;
     private int[] imgages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3,
             R.drawable.image_5, R.drawable.image_6, R.drawable.image_7,
             R.drawable.image_8, R.drawable.image_9, R.drawable.image_10};
+    private String[] titles = {"美女", "美女", "美女", "美女", "美女", "美女", "美女", "美女", "美女"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +37,26 @@ public class UseViewpagerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_use_viewpager);
         viewPager = (ViewPager) this.findViewById(R.id.viewpager);
         views = new ArrayList<>();
+        data_list = new ArrayList<>();
         initDatas();
-        adapter = new ViewPagerAdapter(this,views);
+        adapter = new ViewPagerAdapter(this, views);
         viewPager.setAdapter(adapter);
     }
 
     private void initDatas() {
         for (int i = 0; i < imgages.length; i++) {
             view = LayoutInflater.from(this).inflate(R.layout.viewpager_views, null);
-            image = view.findViewById(R.id.img_view);
-            image.setBackgroundResource(imgages[i]);
+            //image = view.findViewById(R.id.img_view);
+            //image.setBackgroundResource(imgages[i]);
+            gridView = view.findViewById(R.id.grid_view);
+            Map<String, Object> map = new HashMap<>();
+            map.put("images", imgages[i]);
+            map.put("text", titles[i]);
+            data_list.add(map);
+            String[] from = {"images", "text"};
+            int[] to = {R.id.show_img_1, R.id.image_title};
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this, data_list, R.layout.grid_view_item, from, to);
+            gridView.setAdapter(simpleAdapter);
             views.add(view);
         }
     }
