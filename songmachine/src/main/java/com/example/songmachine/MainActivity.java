5c43a1488af805508c593d7e98f559c4a9ae35c0
zhuqg <369793561@qq.com>
2018-01-24 21:09:22 +0800

    2018/01/24 21:08
    1. videoView播放视频，好像效果还是不行，需要调试。

--- songmachine/src/main/java/com/example/songmachine/MainActivity.java
+++ songmachine/src/main/java/com/example/songmachine/MainActivity.java
@@ -2,31 +2,83 @@ package com.example.songmachine;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.example.songmachine.util.StorageCManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
 
     private List<String> mPermissions = new ArrayList<>(); // mPermissions只包含必须要的权限,其他权限可去动态申请
     private static final int KEY_REQUEST_PERMISSION_CODE = 1;
+    public VideoView mVideoVie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+        setupVideo();
         // 当系统为6.0及以上，检查App权限
         if (Build.VERSION.SDK_INT >= 23) {
             checkAppPermission();
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!mVideoVie.isPlaying()) {
+            mVideoVie.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mVideoVie.canPause()) {
+            mVideoVie.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mVideoVie.stopPlayback();
+    }
+
+    private void initUI() {
+        mVideoVie = (VideoView) this.findViewById(R.id.video_main);
+        Log.e("liu","mVideoVie: " + mVideoVie);
+        mVideoVie.setMediaController(new MediaController(this));
+        Uri uri = Uri.parse(StorageCManager.filePath);
+        mVideoVie.setVideoURI(uri);
+        mVideoVie.setOnPreparedListener(this);
+        mVideoVie.setOnCompletionListener(this);
+        mVideoVie.setOnErrorListener(this);
+    }
+
+    /**
+     * 设置本地视频路径的uri,暂时紧作为测试专用
+     */
+    private void setupVideo() {
+
+    }
+
 
     /**
      * 检查App是否拥有权限
@@ -45,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * 请求权限返回结果
+     *
      * @param requestCode
      * @param permissions
      * @param grantResults
@@ -65,5 +118,39 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    /**
+     * 视频准备的监听
+     *
+     * @param mediaPlayer
+     */
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mVideoVie.start();
+    }
+
+    /**
+     * 视频播放完成的监听事件
+     *
+     * @param mediaPlayer
+     */
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        mVideoVie.stopPlayback();
+    }
+
+    /**
+     * 视频播放错误的监听事件
+     *
+     * @param mediaPlayer
+     * @param i
+     * @param i1
+     * @return
+     */
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        mVideoVie.stopPlayback();
+        return false;
     }
 }
---
+++ songmachine/src/main/java/com/example/songmachine/util/StorageCManager.java
@@ -0,0 +1,20 @@
+package com.example.songmachine.util;
+
+import android.annotation.SuppressLint;
+
+/**
+ * Created by Administrator on 2018/1/24.
+ */
+
+public class StorageCManager {
+
+    public static final String TAG = "StorageCManager";
+
+    @SuppressLint("SdCardPath")
+    public static final String filePath = "/mnt/sdcard/1234.mkv"; //临时测试的视频路径
+
+    @SuppressLint("SdCardPath")
+    public static final String filePath1 = "/mnt/sdcard/test.mp4";
+
+
+}
