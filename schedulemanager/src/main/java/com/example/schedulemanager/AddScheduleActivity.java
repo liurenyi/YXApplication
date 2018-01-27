31e5b13402a87ce3c79ad8a2697eade2cf1d4e74
zhuqg <369793561@qq.com>
2018-01-27 18:05:31 +0800

    2018/01/27 18:05
    1. 新增提醒功能，还在完善。（未完成）

--- schedulemanager/src/main/java/com/example/schedulemanager/AddScheduleActivity.java
+++ schedulemanager/src/main/java/com/example/schedulemanager/AddScheduleActivity.java
@@ -1,21 +1,37 @@
 package com.example.schedulemanager;
 
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.schedulemanager.database.Schedule;
 import com.example.schedulemanager.database.ScheduleType;
 import com.example.schedulemanager.util.UtilClass;
+
+import java.lang.reflect.Field;
 
 public class AddScheduleActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,21 +40,22 @@ public class AddScheduleActivity extends AppCompatActivity implements View.OnCli
     public Spinner spinner;
     public ImageView imgOK;
     public ImageView imgCancle;
-
+    public Button btnAlert;
     public String themeString;
     public String remarksString;
     public String typeString;
-
-    private Schedule mShedule;
-    private ScheduleType mScheduleType;
-
+    public Schedule mShedule;
+    public ScheduleType mScheduleType;
+    public Dialog mDialog;
+    public View view;
+    public DatePicker datePicker;
+    public TextView tvSetTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
         initUI();
-
     }
 
     private void initUI() {
@@ -46,12 +63,10 @@ public class AddScheduleActivity extends AppCompatActivity implements View.OnCli
         edtTheme = (EditText) this.findViewById(R.id.edt_theme);
         edtTheme.addTextChangedListener(new ThemeTextWatcher());
         edtRemarks.addTextChangedListener(new RemarksTextWatcher());
-
         imgOK = (ImageView) this.findViewById(R.id.img_save);
         imgCancle = (ImageView) this.findViewById(R.id.img_back);
         imgOK.setOnClickListener(this);
         imgCancle.setOnClickListener(this);
-
         spinner = (Spinner) this.findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -65,6 +80,8 @@ public class AddScheduleActivity extends AppCompatActivity implements View.OnCli
 
             }
         });
+        btnAlert = (Button) this.findViewById(R.id.btn_alert);
+        btnAlert.setOnClickListener(this);
     }
 
     @Override
@@ -72,13 +89,52 @@ public class AddScheduleActivity extends AppCompatActivity implements View.OnCli
         switch (view.getId()) {
             case R.id.img_save:
                 saveAddSchedule();
-
-
                 break;
             case R.id.img_back:
                 finish();
                 break;
+            case R.id.btn_alert:
+                setAlarmAlert();
+                break;
         }
+    }
+
+    /**
+     * 设置提醒时间设置提醒时间
+     */
+    private void setAlarmAlert() {
+        mDialog = new Dialog(this, R.style.ActionSheetDialogStyle);
+        view = LayoutInflater.from(this).inflate(R.layout.layout_alarm_alert, null);
+        datePicker = view.findViewById(R.id.dateAndTimePicker_datePicker);
+        tvSetTime = view.findViewById(R.id.tv_current_set_time);
+        tvSetTime.setText(getResources().getString(R.string.tv_set_alarm_time_text, UtilClass.getNowYear(),
+                UtilClass.getNowMonth() + 1, UtilClass.getNowDat()));
+        tvSetTime.append(UtilClass.getNowDate());
+        if (getResources().getConfiguration().locale.getCountry().equals("CN")) {
+            // 隐藏掉年份显示，隐藏月份显示只需把最后一个getChildAt值设置为1即可
+            ((ViewGroup) ((ViewGroup) datePicker.getChildAt(0)).getChildAt(0)).getChildAt(0).setVisibility(View.GONE);
+        }
+        //setDatePickerDividerColor(datePicker);
+        datePicker.init(UtilClass.getNowYear(), UtilClass.getNowMonth(), UtilClass.getNowDat(), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                tvSetTime.setText(getResources().getString(R.string.tv_set_alarm_time_text, i, i1 + 1, i2));
+                tvSetTime.append(UtilClass.getAppointedDate(i, i1, i2));
+            }
+        });
+        mDialog.setContentView(view);
+        Window window = mDialog.getWindow();
+        WindowManager manager = mDialog.getWindow().getWindowManager();
+        Display display = manager.getDefaultDisplay();
+        if (window != null) {
+            window.setGravity(Gravity.BOTTOM);
+        }
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.width = display.getWidth() - 40;   // 弹出框宽度与屏幕宽度一致
+        window.setAttributes(layoutParams);
+
+        window.setBackgroundDrawableResource(R.drawable.layout_dialog_pop_background); // dialog的背景
+        mDialog.show();
     }
 
     /**
--- schedulemanager/src/main/java/com/example/schedulemanager/util/UtilClass.java
+++ schedulemanager/src/main/java/com/example/schedulemanager/util/UtilClass.java
@@ -1,17 +1,25 @@
 package com.example.schedulemanager.util;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 
 import com.example.schedulemanager.R;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,9 +31,10 @@ import java.util.regex.Pattern;
 public class UtilClass {
 
     private static Date date = new Date();
+    private static Calendar calendar = Calendar.getInstance();
 
     public static String getCurrentDate() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");
         return simpleDateFormat.format(date);
     }
 
@@ -88,6 +97,101 @@ public class UtilClass {
             winParams.flags &= ~bits;
         }
         win.setAttributes(winParams);
+    }
+
+    /**
+     * 设置分割线的颜色
+     *
+     * @param datePicker
+     */
+    private void setDatePickerDividerColor(DatePicker datePicker, Context context) {
+        // Divider changing:
+        // 获取 mSpinners
+        LinearLayout llFirst = (LinearLayout) datePicker.getChildAt(0);
+        // 获取 NumberPicker
+        LinearLayout mSpinners = (LinearLayout) llFirst.getChildAt(0);
+        for (int i = 0; i < mSpinners.getChildCount(); i++) {
+            NumberPicker picker = (NumberPicker) mSpinners.getChildAt(i);
+
+            Field[] declaredFields = NumberPicker.class.getDeclaredFields();
+            for (Field pf : declaredFields) {
+                if (pf.getName().equals("mSelectionDivider")) {
+                    pf.setAccessible(true);
+                    try {
+                        pf.set(picker, new ColorDrawable(context.getResources().getColor(R.color.colorPrimary)));
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                    } catch (Resources.NotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int getNowYear() {
+        return calendar.get(Calendar.YEAR);
+    }
+
+    public static int getNowMonth() {
+        return calendar.get(Calendar.MONTH);
+    }
+
+    public static int getNowDat() {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public static String getNowDate() {
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        if (dayOfWeek == 1) {
+            return "星期日";
+        } else if (dayOfWeek == 2) {
+            return "星期一";
+        } else if (dayOfWeek == 3) {
+            return "星期二";
+        } else if (dayOfWeek == 4) {
+            return "星期三";
+        } else if (dayOfWeek == 5) {
+            return "星期四";
+        } else if (dayOfWeek == 6) {
+            return "星期五";
+        } else if (dayOfWeek == 7) {
+            return "星期六";
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取指定的日期是星期几
+     *
+     * @return
+     */
+    public static String getAppointedDate(int year, int month, int day) {
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        if (dayOfWeek == 1) {
+            return "星期日";
+        } else if (dayOfWeek == 2) {
+            return "星期一";
+        } else if (dayOfWeek == 3) {
+            return "星期二";
+        } else if (dayOfWeek == 4) {
+            return "星期三";
+        } else if (dayOfWeek == 5) {
+            return "星期四";
+        } else if (dayOfWeek == 6) {
+            return "星期五";
+        } else if (dayOfWeek == 7) {
+            return "星期六";
+        } else {
+            return null;
+        }
     }
 
 }
