8f8887721a74f4bab9e7ece14eb3940217596359
Liu renyi <928171620@qq.com>
2018-03-20 09:38:37 +0800

    2018.3.15 1.增加“点歌功能”，可自行点歌 2.优化item显示 3.解决已知问题
    2018.3.14
    1.增加主页播放歌曲可全屏显示功能
    2.修复“已经打开一个预览界面，在点击歌曲报错问题”。
    3.修复“上一首歌曲处于暂停状态，点击切歌，图标不改变。”
    
    2018.3.13
    1.增加静音模式播放预览界面，解决同时播放二个视频的二个声音响起问题。
    2.增加预览模式，点击选中的歌曲，可预览此歌曲信息。
    3.调音功能，增加重低音效果。
    4.首页，显示歌曲上一首，下一曲功能优化。
    
    2018.3.9
    1.增加默认音效设置，音效场景的持久化保存。
    2.优化代码，解决已知bug。
    
    2018.3.8
    1.优化主界面RecyclerView加载显示，更加清爽。
    2.新增调音效果，可调节正在播放的歌曲的音效。
    3.修复一些已知bug。
    
    2018.3.7
    1.主界面实现跑马灯效果，展示当前歌曲信息，以及下一曲歌曲的信息。
    2.优化代码，主界面加载更加流畅。

--- songmachine/src/main/java/com/example/songmachine/util/StorageCManager.java
+++ songmachine/src/main/java/com/example/songmachine/util/StorageCManager.java
@@ -1,50 +1,16 @@
 package com.example.songmachine.util;
 
-import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
-import android.media.MediaMetadataRetriever;
 import android.os.Environment;
-import android.widget.Toast;
-
-import com.example.songmachine.MainActivity;
-import com.example.songmachine.log.Logw;
-
-import java.io.File;
-
-/**
- * Created by Administrator on 2018/1/24.
- */
 
 public class StorageCManager {
 
     public static final String TAG = "StorageCManager";
 
-    @SuppressLint("SdCardPath")
-    public static final String filePath = "/mnt/sdcard/1234.mkv"; //临时测试的视频路径
-
-    /**
-     * 获取视频第一帧
-     *
-     * @return
-     */
-    public static Bitmap createVideoThumbnail(String filePath) {
-        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        File file = new File(filePath);
-        Bitmap bitmap = null;
-        if (file.exists()) {
-            retriever.setDataSource(file.getAbsolutePath());
-            bitmap = retriever.getFrameAtTime();
-            if (bitmap == null) {
-                Logw.e(TAG, "获取第一帧失败");
-            }
-        } else {
-            Logw.e(TAG, "视频文件不存在");
-        }
-        return bitmap;
-    }
+    public static final String filePath = ""; //测试用的uri路径
 
     /**
      * 获取内部sdcard的根目录
+     *
      * @return 返回根目录
      */
     public static String getInnerSDcardPath() {
